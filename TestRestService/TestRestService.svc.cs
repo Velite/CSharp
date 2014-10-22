5dce42d9337f6a1a4000a2f3aafddc802d984f77
Velite <xak1980@yandex.ru>
2014-10-22 17:48:00 +0400

    commit 2

---
+++ TestRestService/TestRestService.svc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+
+namespace TestRestService
+{
+	[AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+	[ServiceContract]
+	public class TestRestService
+	{
+		[OperationContract]
+		[WebGet(UriTemplate = "/", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped)]
+		public string Hello()
+		{
+			return string.Format("Hello world!");
+		}
+
+		[OperationContract]
+		[WebGet(UriTemplate = "/{name}", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped)]
+		public string HelloName(string name)
+		{
+			return string.Format("Hello: {0}", !string.IsNullOrWhiteSpace(name) ? name : "Anonymouse");
+		}
+	}
+
+	public sealed class TestItem
+	{
+		public int Id { get; set; }
+		public DateTime Created { get; set; }
+		public string Comment { get; set; }
+	}
+}
