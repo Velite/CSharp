22fe800866db0babbd76592828899525395ccb80
Velite <xak1980@yandex.ru>
2014-10-22 18:15:37 +0400

    commit 3

--- TestRestService/TestRestService.svc.cs
+++ TestRestService/TestRestService.svc.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.ServiceModel;
 using System.ServiceModel.Activation;
 using System.ServiceModel.Web;
+using Newtonsoft.Json;
 
 namespace TestRestService
 {
@@ -21,6 +24,66 @@ namespace TestRestService
 		public string HelloName(string name)
 		{
 			return string.Format("Hello: {0}", !string.IsNullOrWhiteSpace(name) ? name : "Anonymouse");
+		}
+
+		[OperationContract]
+		[WebGet(UriTemplate = "/Items", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped)]
+		public string GetItems()
+		{
+			List<TestItem> items = new List<TestItem>
+			{
+				new TestItem
+				{
+					Id = 1,
+					Created = DateTime.Now,
+					Comment = "Test 1"
+				},
+				new TestItem
+				{
+					Id = 2,
+					Created = DateTime.Now.AddDays(-1),
+					Comment = "Test 2"
+				},
+				new TestItem
+				{
+					Id = 3,
+					Created = DateTime.Now.AddDays(-2),
+					Comment = "Test 3"
+				}
+			};
+
+			return JsonConvert.SerializeObject(items);
+		}
+
+		[OperationContract]
+		[WebGet(UriTemplate = "/Item/{id}", ResponseFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped)]
+		public string GetItemById(string id)
+		{
+			List<TestItem> items = new List<TestItem>
+			{
+				new TestItem
+				{
+					Id = 1,
+					Created = DateTime.Now,
+					Comment = "Test 1"
+				},
+				new TestItem
+				{
+					Id = 2,
+					Created = DateTime.Now.AddDays(-1),
+					Comment = "Test 2"
+				},
+				new TestItem
+				{
+					Id = 3,
+					Created = DateTime.Now.AddDays(-2),
+					Comment = "Test 3"
+				}
+			};
+
+			int realId = int.Parse(id);
+
+			return realId > 0 ? JsonConvert.SerializeObject(items.AsParallel().Where(i => i.Id == realId)) : "{Ошибка: \"Значение Id < 0\"}";
 		}
 	}
 
